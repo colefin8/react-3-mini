042f52958532f9fd81c505006241ab84adaba205
colefin8 <cole.fin8@gmail.com>
2019-09-26 12:23:55 -0600

    done with in class, not black diamond

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import logo from './mainStreetAuto.svg';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./mainStreetAuto.svg";
+import axios from "axios";
+import "./App.css";
 
 // Toast notification dependencies
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
 class App extends Component {
   constructor(props) {
@@ -29,8 +29,16 @@ class App extends Component {
   }
 
   getVehicles() {
-    // axios (GET)
-    // setState with response -> vehiclesToDisplay
+    axios
+      .get("https://joes-autos.herokuapp.com/api/vehicles")
+      .then(res => {
+        this.setState({ vehiclesToDisplay: res.data });
+        toast.success("Success!");
+      })
+      .catch(err => {
+        console.log(err);
+        toast.error("Failed!");
+      });
   }
 
   getPotentialBuyers() {
@@ -39,6 +47,16 @@ class App extends Component {
   }
 
   sellCar(id) {
+    axios
+      .delete(`https://joes-autos.herokuapp.com/api/vehicles/${id}`)
+      .then(res => {
+        this.setState({ vehiclesToDisplay: res.data.vehicles });
+        toast.success("Car sold!");
+      })
+      .catch(err => {
+        toast.error("Car not sold");
+        console.log(err);
+      });
     // axios (DELETE)
     // setState with response -> vehiclesToDisplay
   }
@@ -58,6 +76,18 @@ class App extends Component {
   }
 
   updatePrice(priceChange, id) {
+    console.log(priceChange, id);
+    axios
+      .put(`https://joes-autos.herokuapp.com/api/vehicles/${id}/${priceChange}`)
+      .then(res => {
+        this.setState({ vehiclesToDisplay: res.data.vehicles });
+        console.log(res.data);
+        toast.success("Successfully updated price!");
+      })
+      .catch(err => {
+        toast.error("Failed at updating price!");
+        console.log(err);
+      });
     // axios (PUT)
     // setState with response -> vehiclesToDisplay
   }
@@ -70,7 +100,16 @@ class App extends Component {
       year: this.year.value,
       price: this.price.value
     };
-
+    axios
+      .post("https://joes-autos.herokuapp.com/api/vehicles", newCar)
+      .then(res => {
+        this.setState({ vehiclesToDisplay: res.data.vehicles });
+        toast.success("New car added!");
+      })
+      .catch(err => {
+        console.log(err);
+        toast.error("Failed to add car");
+      });
     // axios (POST)
     // setState with response -> vehiclesToDisplay
   }
@@ -108,9 +147,9 @@ class App extends Component {
   // Do not edit the code below
   resetData(dataToReset) {
     axios
-      .get('https://joes-autos.herokuapp.com/api/' + dataToReset + '/reset')
+      .get("https://joes-autos.herokuapp.com/api/" + dataToReset + "/reset")
       .then(res => {
-        if (dataToReset === 'vehicles') {
+        if (dataToReset === "vehicles") {
           this.setState({ vehiclesToDisplay: res.data.vehicles });
         } else {
           this.setState({ buyersToDisplay: res.data.buyers });
@@ -131,14 +170,14 @@ class App extends Component {
 
           <button
             className="btn btn-sp"
-            onClick={() => this.updatePrice('up', v.id)}
+            onClick={() => this.updatePrice("up", v.id)}
           >
             Increase Price
           </button>
 
           <button
             className="btn btn-sp"
-            onClick={() => this.updatePrice('down', v.id)}
+            onClick={() => this.updatePrice("down", v.id)}
           >
             Decrease Price
           </button>
@@ -182,14 +221,14 @@ class App extends Component {
 
           <button
             className="header-btn1 btn"
-            onClick={() => this.resetData('vehicles')}
+            onClick={() => this.resetData("vehicles")}
           >
             Reset Vehicles
           </button>
 
           <button
             className="header-btn2 btn"
-            onClick={() => this.resetData('buyers')}
+            onClick={() => this.resetData("buyers")}
           >
             Reset Buyers
           </button>
